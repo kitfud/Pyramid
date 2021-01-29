c09479fbc2b5da4e3b0412f85a2068d26c994a05
Kit Fuderich <kitfuderich@gmail.com>
2021-01-29 23:48:04 +0000

    Image file/cards fixed and pyramid created

--- card.py
+++ card.py
@@ -6,6 +6,10 @@ class Card(pygame.sprite.Sprite):
     self.rank = rank
     self.suit = suit 
     self.faceup = False 
-    self.images = {True: pygame.image.load("images/cards/{} {}.png".format(suit,rank)),False:pygame.image.load("images/cards/Back Blue 1.png")}
+    self.images = {True:pygame.image.load("images/cards/{} {}.png".format(suit,rank)),False:pygame.image.load("images/cards/Back Blue 1.png")}
     self.image = self.images[self.faceup]
     self.rect = self.image.get_rect()
+  
+  def flip(self):
+    self.faceup= not self.faceup
+    self.image = self.images[self.faceup]
--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import pygame, sys
 from pygame.locals import *
+from deck import *
 
+deck = None
 pygame.init()
-
+board = pygame.sprite.Group()
 size = (width,height) = (700,500)
 
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 bg_color = (0,102,0)
 
+def init():
+  global deck
+  deck = Deck()
+  for i in range(7):
+    for j in range(i+1):
+      card = deck.deal()
+      card.rect.midtop = (width//2-40*i + 80*j,30 + 30*i)
+      card.flip()
+      board.add(card)
+
 def main():
   global screen 
+  init()
   while True:
     clock.tick(60)
     for event in pygame.event.get():
@@ -22,6 +35,7 @@ def main():
         elif event.key == K_ESCAPE:
           screen = pygame.display.set_mode(size)
       screen.fill(bg_color)
+      board.draw(screen)
       pygame.display.flip()
 
 if __name__ =="__main__":
