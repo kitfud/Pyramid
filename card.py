1e27232f2228df536d2ebad40bc983f22d418d07
Kit Fuderich <kitfuderich@gmail.com>
2021-01-29 21:07:12 +0000

    Card Class created

---
+++ card.py
@@ -0,0 +1,11 @@
+import pygame
+
+class Card(pygame.sprite.Sprite):
+  def __init__(self,rank,suit):
+    super().__init__()
+    self.rank = rank
+    self.suit = suit 
+    self.faceup = False 
+    self.images = {True: pygame.image.load("images/cards/{} {}.png".format(suit,rank)),False:pygame.image.load("images/cards/Back Blue 1.png")}
+    self.image = self.images[self.faceup]
+    self.rect = self.image.get_rect()
