d742694a90243cfbad0e5bf72d0049f784476eea
Kit Fuderich <kitfuderich@gmail.com>
2021-01-30 00:01:26 +0000

    Card clicked select and boarder highlight

--- main.py
+++ main.py
@@ -11,6 +11,20 @@ screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 bg_color = (0,102,0)
 
+selected_card = pygame.sprite.GroupSingle()
+
+def check_sprite_clicked(x,y):
+  card_clicked = None
+  for card in board:
+    if card.rect.collidepoint(x,y):
+      card_clicked = card
+    if card_clicked is not None:
+      hit_list = pygame.sprite.spritecollide(card_clicked,board,False)
+      for card in hit_list:
+        if card.rect.y > card_clicked.rect.y:
+          return
+      selected_card.add(card_clicked)
+
 def init():
   global deck
   deck = Deck()
@@ -34,8 +48,14 @@ def main():
           screen = pygame.display.set_mode(size,FULLSCREEN)
         elif event.key == K_ESCAPE:
           screen = pygame.display.set_mode(size)
+      if event.type == MOUSEBUTTONDOWN:
+        if event.button ==1:
+          x,y = event.pos
+          check_sprite_clicked(x,y)
       screen.fill(bg_color)
       board.draw(screen)
+      if len(selected_card) == 1:
+        pygame.draw.rect(screen,(204,173,0),selected_card.sprite.rect,3)
       pygame.display.flip()
 
 if __name__ =="__main__":
